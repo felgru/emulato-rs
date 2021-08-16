1d016feb36b1c02ce19cb1f247b1f924e663b4d2
Felix Gruber <felgru@posteo.net>
2021-08-16 23:45:16 +0200

    GameBoy: Increment TIMA with falling edge detector
    This handles more weird corner cases than before.

--- src/game_boy/timer.rs
+++ src/game_boy/timer.rs
@@ -1,48 +1,66 @@
 pub struct Timer {
     clock: u16,
-    timer_counter: i16,
+    timer_counter: u16,
+    timer_trigger: u16,
+    timer_trigger_bit: bool,
     timer: u8,
     modulo: u8,
     control: u8,
+    stopped: bool,
 }
 
 impl Default for Timer {
     fn default() -> Self {
         Timer {
             clock: 0,
-            timer_counter: 1024,
+            timer_counter: 0,
+            timer_trigger: 1 << 9,
+            timer_trigger_bit: false,
             timer: 0,
             modulo: 0,
             control: 0xF8,  // Only lowest 3 bits are used, rest is 1.
+            stopped: false,
         }
     }
 }
 
 impl Timer {
     pub fn step(&mut self, cycles: usize) -> bool {
-        // TODO: Don't update when in STOP mode.
-        self.clock = self.clock.wrapping_add(cycles as u16);
-        // TODO: According to this schematic
-        //       https://gbdev.io/pandocs/#timer-global-circuit
-        //       timer_counter is coupled to clock.
-        if !self.is_timer_enabled() {
+        if self.stopped {
             return false;
         }
-        let mut interrupt = false;
-        self.timer_counter -= cycles as i16;
-        // TODO: Can this overflow multiple times when processing a slow
+        self.clock = self.clock.wrapping_add(cycles as u16);
+        // TODO: Can the timer increment multiple times when processing a slow
         //       instruction?
-        while self.timer_counter <= 0 {
-            self.timer_counter += self.ticks_per_timer_increment();
+        // Coupling of clock and timer according to
+        // https://gbdev.io/pandocs/Timer_Obscure_Behaviour.html
+        let new_timer_trigger_bit
+            = self.is_timer_enabled()
+              && (self.clock & self.timer_trigger) != 0;
+        let interrupt = if self.timer_trigger_bit && !new_timer_trigger_bit {
             let (new_timer, overflow) = self.timer.overflowing_add(1);
+            let mut interrupt = false;
             self.timer = if !overflow {
                 new_timer
             } else {
                 interrupt = true;
                 self.modulo
             };
-        }
+            interrupt
+        } else {
+            false
+        };
+        self.timer_trigger_bit = new_timer_trigger_bit;
         interrupt
+    }
+
+    pub fn stop_clock(&mut self) {
+        self.stopped = true;
+        self.reset_divider();
+    }
+
+    pub fn start_clock(&mut self) {
+        self.stopped = false;
     }
 
     pub fn get_divider(&self) -> u8 {
@@ -51,6 +69,7 @@ impl Timer {
 
     pub fn reset_divider(&mut self) {
         self.clock = 0;
+        self.timer_counter = 0;
     }
 
     pub fn get_timer(&self) -> u8 {
@@ -76,19 +95,22 @@ impl Timer {
     pub fn set_control(&mut self, value: u8) {
         // The unused bits 3–7 are always 1.
         self.control = value | 0xF8;
+        // TODO: There are probably weird corner cases to handle
+        //       if the new trigger makes the timer overflow.
+        self.timer_trigger = self.get_timer_trigger_bit();
     }
 
     fn is_timer_enabled(&self) -> bool {
         self.control & 0x4 != 0
     }
 
-    fn ticks_per_timer_increment(&self) -> i16 {
-        // https://gbdev.io/pandocs/#ff07-tac-timer-control-r-w
+    fn get_timer_trigger_bit(&self) -> u16 {
+        // https://gbdev.io/pandocs/Timer_and_Divider_Registers.html#ff07---tac---timer-control-rw
         match self.control & 0x3 {
-            0 => 1024,
-            1 =>   16,
-            2 =>   64,
-            3 =>  256,
+            0 => 1 << 9,
+            1 => 1 << 3,
+            2 => 1 << 5,
+            3 => 1 << 7,
             _ => unreachable!(),
         }
     }
