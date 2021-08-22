56b8b38f82659c3f6500101459a209039727739d
Felix Gruber <felgru@posteo.net>
2021-08-22 11:35:11 +0200

    GameBoy: Emulate exact timing of OAM DMA transfer

--- src/game_boy/memory.rs
+++ src/game_boy/memory.rs
@@ -22,6 +22,11 @@ use super::timer::Timer;
 /// 0xFF80–0xFFFE  HRAM  High RAM Area (targetted by special load instructions)
 /// 0xFFFF         IE Register  Interrupt Enabled Register
 pub struct MemoryBus {
+    memory: Memory,
+    dma_transfer: Option<OamDmaTransfer>,
+}
+
+struct Memory {
     memory: [u8; 0x10000],
     cartridge: Cartridge,
     boot_rom: Option<[u8; 0x100]>,
@@ -31,6 +36,210 @@ pub struct MemoryBus {
 
 impl MemoryBus {
     pub fn new(cartridge: Cartridge, boot_rom: [u8; 0x100]) -> Self {
+        Self{
+            memory: Memory::new(cartridge, boot_rom),
+            dma_transfer: None,
+        }
+    }
+
+    pub fn read8(&self, address: u16) -> u8 {
+        if self.dma_transfer.is_active()
+           && address < 0xFF80 && address != 0xFF46 {
+            return 0xFF;
+        }
+        self.memory.read8(address)
+    }
+
+    pub fn write8(&mut self, address: u16, value: u8) {
+        if address == 0xFF46 {
+            // Object Attribute Memory (OAM) DMA Control Register
+            // This will take 160 cycles during which the CPU
+            // continues execution but only has access to HRAM.
+            self.memory.write8(address, value);
+            self.dma_transfer
+                = Some(OamDmaTransfer::new(value,
+                                           self.dma_transfer.is_some()));
+        } else if !self.dma_transfer.is_active() || address >= 0xFF80 {
+            // OAM DMA transfer block all memory access except for 0xFF46
+            // and HRAM.
+            self.memory.write8(address, value);
+        }
+    }
+
+    pub fn read16(&self, address: u16) -> u16 {
+        self.read8(address) as u16 + ((self.read8(address+1) as u16) << 8)
+    }
+
+    pub fn write16(&mut self, address: u16, value: u16) {
+        self.write8(address, value as u8);
+        self.write8(address+1, (value >> 8) as u8);
+    }
+
+    pub fn step(&mut self, cycles: usize) {
+        if let Some(dma_transfer) = self.dma_transfer.as_mut() {
+            for _ in (0..cycles).step_by(4) {
+                if dma_transfer.step(&mut self.memory) {
+                    eprintln!("Stopping DMA transfer.");
+                    self.dma_transfer = None;
+                    break
+                }
+            }
+        }
+        if self.memory.timer.step(cycles) {
+            // request Timer interrupt
+            self.memory.memory[0xFF0F] |= 4;
+        }
+    }
+
+    pub fn lcdc(&self) -> LcdControl {
+        self.memory.lcdc()
+    }
+
+    pub fn lcd_status(&mut self) -> LcdStatus {
+        self.memory.lcd_status()
+    }
+
+    pub fn set_lcd_mode(&mut self, mode: LcdMode) {
+        self.memory.set_lcd_mode(mode);
+    }
+
+    pub fn scy(&self) -> u8 {
+        self.memory.scy()
+    }
+
+    pub fn scx(&self) -> u8 {
+        self.memory.scx()
+    }
+
+    pub fn ly(&self) -> u8 {
+        self.memory.ly()
+    }
+
+    pub fn set_ly(&mut self, ly: u8) {
+        self.memory.set_ly(ly);
+    }
+
+    pub fn lyc(&self) -> u8 {
+        self.memory.lyc()
+    }
+
+    pub fn wy(&self) -> u8 {
+        self.memory.wy()
+    }
+
+    pub fn wx(&self) -> u8 {
+        self.memory.wx()
+    }
+
+    pub fn bg_palette(&self) -> MonochromePalette {
+        self.memory.bg_palette()
+    }
+
+    pub fn obj_palette0(&self) -> MonochromePalette {
+        self.memory.obj_palette0()
+    }
+
+    pub fn obj_palette1(&self) -> MonochromePalette {
+        self.memory.obj_palette1()
+    }
+
+    /// Set pressed JoyPad keys
+    ///
+    /// Keypresses are given as a bitmap with 1 bit per button,
+    /// which is 1 if pressed and 0 if unpressed.
+    ///
+    /// Bit  Button
+    /// ---  -------
+    /// 0    Right
+    /// 1    Left
+    /// 2    Up
+    /// 3    Down
+    /// 4    A
+    /// 5    B
+    /// 6    Select
+    /// 7    Start
+    pub fn set_key_presses(&mut self, presses: u8) -> bool {
+        self.memory.set_key_presses(presses)
+    }
+
+    pub fn dump_tile_data<W: std::io::Write>(
+            &self, buffer: &mut W) -> std::io::Result<()> {
+        let tile_size = 2 * 8;
+        let num_tiles = (0x9800 - 0x8000) / tile_size;
+        // print tile data in rows of 16 tiles each
+        let tiles_per_row = 16;
+        let num_rows = num_tiles / tiles_per_row;
+        writeln!(buffer, "P2")?;
+        writeln!(buffer, "{} {}", tiles_per_row*8, num_rows*8)?;
+        writeln!(buffer, "3")?;
+        let mut row_start = 0x8000;
+        for _tile_row in 0..num_rows {
+            for row in 0..8 {
+                for tile_col in 0..tiles_per_row {
+                    let tile = self.read16(row_start + 2 * row
+                                           + tile_size * tile_col);
+                    let p = ((tile >> 14) & 0b10) | ((tile >> 7) & 1);
+                    write!(buffer, "{}", 3 - p)?;
+                    for i in 1..8 {
+                        let p = ((tile >> 14-i) & 0b10) | ((tile >> 7-i) & 1);
+                        write!(buffer, " {}", 3 - p)?;
+                    }
+                    writeln!(buffer)?;
+                }
+            }
+            row_start += tiles_per_row * tile_size;
+        }
+        Ok(())
+    }
+
+    pub fn dump_bg<W: std::io::Write>(&self,
+                                      buffer: &mut W) -> std::io::Result<()> {
+        let lcdc = self.lcdc();
+        let palette = self.bg_palette().as_array();
+        let tile_map_start = lcdc.bg_tilemap_start();
+        writeln!(buffer, "P2")?;
+        writeln!(buffer, "256 256")?;
+        writeln!(buffer, "3")?;
+        let mut tiles: [u8; 32] = [0; 32];
+        for tile_row in 0..32 {
+            for tile_col in 0..32u16 {
+                let tile
+                    = self.read8(tile_map_start + 32 * tile_row + tile_col);
+                tiles[tile_col as usize] = tile;
+            }
+            for row in 0..8 {
+                for tile_col in 0..32 {
+                    let tile_address
+                        = lcdc.get_bg_or_window_tile_address(tiles[tile_col]);
+                    let tile = self.read16(tile_address + 2 * row);
+                    let index = ((tile >> 14) & 0b10)
+                              | ((tile >> 7) & 1);
+                    let p = palette[index as usize];
+                    write!(buffer, "{}", 3 - p)?;
+                    for i in 1..8 {
+                        let index = ((tile >> 14-i) & 0b10)
+                                  | ((tile >> 7-i) & 1);
+                        let p = palette[index as usize];
+                        write!(buffer, " {}", 3 - p)?;
+                    }
+                    writeln!(buffer)?;
+                }
+            }
+        }
+        Ok(())
+    }
+
+    pub fn get_requested_interrupts(&self) -> u8 {
+        self.memory.get_requested_interrupts()
+    }
+
+    pub fn handle_interrupts(&mut self) -> Option<InterruptAddress> {
+        self.memory.handle_interrupts()
+    }
+}
+
+impl Memory {
+    fn new(cartridge: Cartridge, boot_rom: [u8; 0x100]) -> Self {
         let mut memory = [0; 0x10000];
         memory[0xFF00] = 0xCF;  // upper two bits of JoyPad always 1
         memory[0xFF0F] = 0xE0;  // highest three bits of IF always 1
@@ -44,7 +253,7 @@ impl MemoryBus {
         }
     }
 
-    pub fn read8(&self, address: u16) -> u8 {
+    fn read8(&self, address: u16) -> u8 {
         match address {
             0x0000..=0x00FF => { // Boot ROM / ROM Bank 0
                 if let Some(ref boot_rom) = self.boot_rom {
@@ -152,7 +361,7 @@ impl MemoryBus {
         }
     }
 
-    pub fn write8(&mut self, address: u16, value: u8) {
+    fn write8(&mut self, address: u16, value: u8) {
         match address {
             0x0000..=0x7FFF => { // Cartridge ROM
                 self.cartridge.write8(address, value);
@@ -250,26 +459,9 @@ impl MemoryBus {
                     }
                     0xFF46 => {
                         // Object Attribute Memory (OAM) DMA Control Register
-                        // TODO: This is a simplification of the DMA transfer.
-                        //       Normally it would take 160 cycles during which
-                        //       the CPU continues execution but only has
-                        //       access to HRAM.
+                        // This will take 160 cycles during which the CPU
+                        // continues execution but only has access to HRAM.
                         self.memory[address as usize] = value;
-                        eprintln!("OAM transfer from {0:0>2X}00–{0:0>2X}9F.",
-                                  value);
-                        // ECHO RAM, DMA, etc. remap to WRAM.
-                        let value = if value >= 0xE0 {
-                            value - 0xE0 + 0xC0
-                        } else {
-                            value
-                        };
-                        let source_start = (value as u16) << 8;
-                        for (source, target)
-                            in (source_start..=source_start+0x9F)
-                                .zip(0xFE00..=0xFE9F) {
-                            let value = self.read8(source);
-                            self.write8(target, value);
-                        }
                     }
                     0xFF47..=0xFF49 => {
                         // 0xFF47: BGP (BG Palette Data)
@@ -311,50 +503,34 @@ impl MemoryBus {
         }
     }
 
-    pub fn read16(&self, address: u16) -> u16 {
-        self.read8(address) as u16 + ((self.read8(address+1) as u16) << 8)
-    }
-
-    pub fn write16(&mut self, address: u16, value: u16) {
-        self.write8(address, value as u8);
-        self.write8(address+1, (value >> 8) as u8);
-    }
-
-    pub fn step(&mut self, cycles: usize) {
-        if self.timer.step(cycles) {
-            // request Timer interrupt
-            self.memory[0xFF0F] |= 4;
-        }
-    }
-
-    pub fn lcdc(&self) -> LcdControl {
+    fn lcdc(&self) -> LcdControl {
         LcdControl{flags: self.memory[0xFF40]}
     }
 
-    pub fn lcd_status(&mut self) -> LcdStatus {
+    fn lcd_status(&mut self) -> LcdStatus {
         LcdStatus{flags: &mut self.memory[0xFF41]}
     }
 
-    pub fn set_lcd_mode(&mut self, mode: LcdMode) {
+    fn set_lcd_mode(&mut self, mode: LcdMode) {
         if self.lcd_status().set_mode(mode) {
             // Request Stat interrupt.
             self.memory[0xFF0F] |= 2;
         }
     }
 
-    pub fn scy(&self) -> u8 {
+    fn scy(&self) -> u8 {
         self.memory[0xFF42]
     }
 
-    pub fn scx(&self) -> u8 {
+    fn scx(&self) -> u8 {
         self.memory[0xFF43]
     }
 
-    pub fn ly(&self) -> u8 {
+    fn ly(&self) -> u8 {
         self.memory[0xFF44]
     }
 
-    pub fn set_ly(&mut self, ly: u8) {
+    fn set_ly(&mut self, ly: u8) {
         self.memory[0xFF44] = ly;
         let equal = ly == self.lyc();
         self.lcd_status().update_lyc_eq_ly(equal);
@@ -364,27 +540,27 @@ impl MemoryBus {
         }
     }
 
-    pub fn lyc(&self) -> u8 {
+    fn lyc(&self) -> u8 {
         self.memory[0xFF45]
     }
 
-    pub fn wy(&self) -> u8 {
+    fn wy(&self) -> u8 {
         self.memory[0xFF4A]
     }
 
-    pub fn wx(&self) -> u8 {
+    fn wx(&self) -> u8 {
         self.memory[0xFF4B]
     }
 
-    pub fn bg_palette(&self) -> MonochromePalette {
+    fn bg_palette(&self) -> MonochromePalette {
         self.memory[0xFF47].into()
     }
 
-    pub fn obj_palette0(&self) -> MonochromePalette {
+    fn obj_palette0(&self) -> MonochromePalette {
         self.memory[0xFF48].into()
     }
 
-    pub fn obj_palette1(&self) -> MonochromePalette {
+    fn obj_palette1(&self) -> MonochromePalette {
         self.memory[0xFF49].into()
     }
 
@@ -403,7 +579,7 @@ impl MemoryBus {
     /// 5    B
     /// 6    Select
     /// 7    Start
-    pub fn set_key_presses(&mut self, presses: u8) -> bool {
+    fn set_key_presses(&mut self, presses: u8) -> bool {
         self.joypad = presses;
         self.update_joypad_register()
     }
@@ -434,84 +610,17 @@ impl MemoryBus {
         }
     }
 
-    pub fn disable_boot_rom(&mut self) {
+    fn disable_boot_rom(&mut self) {
         self.boot_rom = None;
     }
 
-    pub fn dump_tile_data<W: std::io::Write>(
-            &self, buffer: &mut W) -> std::io::Result<()> {
-        let tile_size = 2 * 8;
-        let num_tiles = (0x9800 - 0x8000) / tile_size;
-        // print tile data in rows of 16 tiles each
-        let tiles_per_row = 16;
-        let num_rows = num_tiles / tiles_per_row;
-        writeln!(buffer, "P2")?;
-        writeln!(buffer, "{} {}", tiles_per_row*8, num_rows*8)?;
-        writeln!(buffer, "3")?;
-        let mut row_start = 0x8000;
-        for _tile_row in 0..num_rows {
-            for row in 0..8 {
-                for tile_col in 0..tiles_per_row {
-                    let tile = self.read16(row_start + 2 * row
-                                           + tile_size * tile_col);
-                    let p = ((tile >> 14) & 0b10) | ((tile >> 7) & 1);
-                    write!(buffer, "{}", 3 - p)?;
-                    for i in 1..8 {
-                        let p = ((tile >> 14-i) & 0b10) | ((tile >> 7-i) & 1);
-                        write!(buffer, " {}", 3 - p)?;
-                    }
-                    writeln!(buffer)?;
-                }
-            }
-            row_start += tiles_per_row * tile_size;
-        }
-        Ok(())
-    }
-
-    pub fn dump_bg<W: std::io::Write>(&self,
-                                      buffer: &mut W) -> std::io::Result<()> {
-        let lcdc = self.lcdc();
-        let palette = self.bg_palette().as_array();
-        let tile_map_start = lcdc.bg_tilemap_start();
-        writeln!(buffer, "P2")?;
-        writeln!(buffer, "256 256")?;
-        writeln!(buffer, "3")?;
-        let mut tiles: [u8; 32] = [0; 32];
-        for tile_row in 0..32 {
-            for tile_col in 0..32u16 {
-                let tile
-                    = self.read8(tile_map_start + 32 * tile_row + tile_col);
-                tiles[tile_col as usize] = tile;
-            }
-            for row in 0..8 {
-                for tile_col in 0..32 {
-                    let tile_address
-                        = lcdc.get_bg_or_window_tile_address(tiles[tile_col]);
-                    let tile = self.read16(tile_address + 2 * row);
-                    let index = ((tile >> 14) & 0b10)
-                              | ((tile >> 7) & 1);
-                    let p = palette[index as usize];
-                    write!(buffer, "{}", 3 - p)?;
-                    for i in 1..8 {
-                        let index = ((tile >> 14-i) & 0b10)
-                                  | ((tile >> 7-i) & 1);
-                        let p = palette[index as usize];
-                        write!(buffer, " {}", 3 - p)?;
-                    }
-                    writeln!(buffer)?;
-                }
-            }
-        }
-        Ok(())
-    }
-
-    pub fn get_requested_interrupts(&self) -> u8 {
+    fn get_requested_interrupts(&self) -> u8 {
         let requests = self.read8(0xFF0F);
         let enabled_interrupts = self.read8(0xFFFF);
         requests & enabled_interrupts & 0x1F
     }
 
-    pub fn handle_interrupts(&mut self) -> Option<InterruptAddress> {
+    fn handle_interrupts(&mut self) -> Option<InterruptAddress> {
         let mut requests = self.read8(0xFF0F);
         let enabled_interrupts = self.read8(0xFFFF);
         let interrupts = requests & enabled_interrupts & 0x1F;
@@ -684,4 +793,62 @@ pub enum InterruptAddress {
     TIMER = 0x50,
     SERIAL = 0x58,
     JOYPAD = 0x60,
+}
+
+struct OamDmaTransfer {
+    address: u16,
+    pre_transfer_countdown: u8,
+    active_transfer: bool,
+}
+
+impl OamDmaTransfer {
+    fn new(upper_address: u8, restarting: bool) -> Self {
+        eprintln!("OAM transfer from {0:0>2X}00–{0:0>2X}9F requested.",
+                  upper_address);
+        // ECHO RAM, DMA, etc. remap to WRAM.
+        let upper_address = if upper_address >= 0xE0 {
+            upper_address - 0xE0 + 0xC0
+        } else {
+            upper_address
+        };
+        Self {
+            address: (upper_address as u16) << 8,
+            // Countdown calibrated with Mooneye oam_dma_timing test ROM.
+            pre_transfer_countdown: 3,
+            active_transfer: restarting,
+        }
+    }
+
+    /// Copy one OAM byte.
+    ///
+    /// Return whether OAM DMA transfer has finished.
+    fn step(&mut self, memory: &mut Memory) -> bool {
+        if self.pre_transfer_countdown > 0 {
+            self.pre_transfer_countdown -= 1;
+            if self.pre_transfer_countdown == 0 {
+                self.active_transfer = true;
+            }
+            return false;
+        }
+        let value = memory.read8(self.address);
+        let target = 0xFE00 | (self.address & 0xFF);
+        memory.write8(target, value);
+        self.address += 1;
+        target >= 0xFE9F
+    }
+
+    fn is_active(&self) -> bool {
+        self.active_transfer
+    }
+}
+
+trait DmaTestActive {
+    fn is_active(&self) -> bool;
+}
+
+impl DmaTestActive for Option<OamDmaTransfer> {
+    fn is_active(&self) -> bool {
+        self.as_ref().map(|dma| dma.is_active())
+                     .unwrap_or(false)
+    }
 }
